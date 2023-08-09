328f591dfa9840e3e7aa4dec447171d2389068d9
Ngoy banza <dngoyb@gmail.com>
2023-08-09 19:01:49 +0200

    Button component created and tailwindcss added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import Button from './components/Button';
+
 function App() {
-	return <div>App</div>;
+	return (
+		<div>
+			<div>
+				<Button success rounded outlined>
+					Button 1
+				</Button>
+			</div>
+			<div>
+				<Button danger outlined>
+					Click me!
+				</Button>
+			</div>
+			<div>
+				<Button>Button 2</Button>
+			</div>
+			<div>
+				<Button>BUTTON 3</Button>
+			</div>
+			<div>
+				<Button>Now Click</Button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
