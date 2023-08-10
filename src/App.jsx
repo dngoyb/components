e5bdf3696fc5bc008e1589bfdf6afa8376297bcb
Ngoy banza <dngoyb@gmail.com>
2023-08-10 13:18:09 +0200

    Icons added to the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
+import { GoBell, GoDatabase, GoCloudOffline } from 'react-icons/go';
 import Button from './components/Button';
 
 function App() {
 	return (
-		<div>
-			<div>
-				<Button primary rounded outlined>
+		<div className='px-5'>
+			<div className='py-2 '>
+				<Button primary rounded>
+					<GoBell className='mr-1' />
 					Button 1
 				</Button>
 			</div>
-			<div>
+			<div className='py-2 '>
 				<Button success outlined>
+					<GoCloudOffline />
 					Click me!
 				</Button>
 			</div>
-			<div>
-				<Button warning>Button 2</Button>
+			<div className='py-2 '>
+				<Button warning>
+					<GoDatabase />
+					Button 2
+				</Button>
 			</div>
-			<div>
-				<Button secondary>BUTTON 3</Button>
+			<div className='py-2 '>
+				<Button secondary outlined>
+					BUTTON 3
+				</Button>
 			</div>
-			<div>
+			<div className='py-2 '>
 				<Button danger rounded>
 					Now Click
 				</Button>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,14 +10,23 @@ function Button({
 	outlined,
 	rounded,
 }) {
-	const classes = className('px-3 py-1.5 border', {
-		'border-blue-600 bg-blue-500 text-white': primary,
-		'border-gray-900 bg-gray-900 text-white': secondary,
-		'border-green-600 bg-green-500 text-white': success,
-		'border-yellow-400 bg-yellow-400 text-white': warning,
-		'border-red-600 bg-red-500 text-white': danger,
-		'rounded-full': rounded,
-	});
+	const classes = className(
+		'flex items-center px-3 py-1.5 border text-white ',
+		{
+			'border-blue-600 bg-blue-500 ': primary,
+			'border-gray-900 bg-gray-900 ': secondary,
+			'border-green-600 bg-green-500 ': success,
+			'border-yellow-400 bg-yellow-400 ': warning,
+			'border-red-600 bg-red-500 ': danger,
+			'rounded-full': rounded,
+			'bg-white': outlined,
+			'text-blue-500': outlined && primary,
+			'text-gray-900': outlined && secondary,
+			'text-green-500': outlined && success,
+			'text-yellow-500': outlined && warning,
+			'text-red-500': outlined && danger,
+		}
+	);
 	return <button className={classes}>{children}</button>;
 }
 
