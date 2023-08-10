63f970a2b6f15ac643e901ed79f4626c64544e73
Ngoy banza <dngoyb@gmail.com>
2023-08-10 19:57:41 +0200

    click to expand working

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import Accordion from './components/Accordion';
 function App() {
 	const items = [
 		{
-			id: 1,
-			label: 'Title 1',
-			content: 'You can use title 1 whenever you want',
+			id: 'l2kj5',
+			label: 'Can I use React on a project?',
+			content:
+				'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
 		},
 		{
-			id: 2,
-			label: 'Title 2',
-			content: 'You can use title 2 whenever you want',
+			id: 'lk2j35lkj',
+			label: 'Can I use Javascript on a project?',
+			content:
+				'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
 		},
 		{
-			id: 3,
-			label: 'Title ',
-			content: 'You can use title 3 whenever you want',
+			id: 'l1kj2i0g',
+			label: 'Can I use CSS on a project?',
+			content:
+				'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
 		},
 	];
 	return <Accordion items={items} />;
