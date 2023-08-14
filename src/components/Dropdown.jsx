1ce48941bdb5e1417835b1074f5374b2de7a8765
Ngoy banza <dngoyb@gmail.com>
2023-08-14 20:15:11 +0200

    Adding css

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
 
-function Dropdown({ options }) {
+function Dropdown({ options, value, onChange }) {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const handleOptionClick = (option) => {
 		setIsOpen(false);
-		console.log(option);
+		onChange(option);
 	};
 
 	const renderedOptions = options.map((option) => {
 		return (
-			<div key={option.value} onClick={() => handleOptionClick(option)}>
+			<div
+				className='hover:bg-sky-100 rounded cursor-pointer p-1'
+				key={option.value}
+				onClick={() => handleOptionClick(option)}>
 				{option.label}
 			</div>
 		);
@@ -19,10 +23,24 @@ function Dropdown({ options }) {
 	const handleClick = () => {
 		setIsOpen((current) => !isOpen);
 	};
+
 	return (
-		<div>
-			<div onClick={handleClick}>Select...</div>
-			{isOpen && <div>{renderedOptions}</div>}
+		<div className='w-48 relative'>
+			<div
+				className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full'
+				onClick={handleClick}>
+				{value?.label || 'Select...'}
+				{isOpen ? (
+					<GoChevronDown className='text-xl' />
+				) : (
+					<GoChevronLeft className='text-xl' />
+				)}
+			</div>
+			{isOpen && (
+				<div className='absolute top-full border rounded p-3 shadow bg-white w-full'>
+					{renderedOptions}
+				</div>
+			)}
 		</div>
 	);
 }
