c50a42924768e1392b309f8a4fa027aeb7acb232
Ngoy banza <dngoyb@gmail.com>
2023-08-18 14:01:49 +0200

    CreatePortal to fix absolute position

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,15 @@
+import ReactDOM from 'react-dom';
+
 function Modal() {
-	return <div>Modal</div>;
+	console.log(ReactDOM);
+	return ReactDOM.createPortal(
+		<div>
+			<div className='absolute inset-0 bg-gray-300 opacity-80'></div>
+			<div className='absolute inset-40 p-10 bg-white'>I am a Modal</div>
+		</div>,
+		document.querySelector('.modal-container')
+	);
+	return;
 }
 
 export default Modal;
