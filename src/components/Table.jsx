80b7b95feed126d3616f026dffb8144a54963dbc
Ngoy banza <dngoyb@gmail.com>
2023-08-19 17:11:14 +0200

    Adding Config Array to controle the table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,23 +1,25 @@
-function Table({ data }) {
+function Table({ data, config }) {
 	const renderedRows = data.map((fruit) => {
+		const renderedCells = config.map((column) => {
+			return (
+				<td className='p-2' key={column.label}>
+					{column.render(fruit)}
+				</td>
+			);
+		});
 		return (
 			<tr className='border-b' key={fruit.name}>
-				<td className='p-3'>{fruit.name}</td>
-				<td className='p-3'>
-					<div className={`p-3 m-2 ${fruit.color}`}></div>
-				</td>
-				<td className='p-3'>{fruit.score}</td>
+				{renderedCells}
 			</tr>
 		);
+	});
+	const renderedHeaders = config.map((column) => {
+		return <th key={column.label}>{column.label}</th>;
 	});
 	return (
 		<table className='table-auto border-spacing-2'>
 			<thead>
-				<tr className='border-b-2'>
-					<th>Fruit</th>
-					<th>Color</th>
-					<th>Score</th>
-				</tr>
+				<tr className='border-b-2'>{renderedHeaders}</tr>
 			</thead>
 			<tbody>{renderedRows}</tbody>
 		</table>
