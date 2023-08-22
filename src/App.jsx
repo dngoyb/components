09ae235319afdd49090e01c1fc6fd35705abdb4e
Ngoy banza <dngoyb@gmail.com>
2023-08-22 16:31:25 +0200

    useCouter component with custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import DropdownPage from './pages/DropdownPage';
 import ButtonPage from './pages/ButtonPage';
 import ModalPage from './pages/ModalPage';
 import TablePage from './pages/TablePage';
+import CounterPage from './pages/CounterPage';
 
 function App() {
 	return (
@@ -23,8 +24,9 @@ function App() {
 				<Route path='/modal'>
 					<ModalPage />
 				</Route>
-				<Route path='/table'>
-					<TablePage />
+
+				<Route path='/counter'>
+					<CounterPage initialCount={10} />
 				</Route>
 			</div>
 		</div>
